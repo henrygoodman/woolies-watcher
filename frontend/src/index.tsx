bfe817b07c411df77f9833a14376e0b14e7d3ed1
Henry Goodman <Henry.T.Goodman@student.uts.edu.au>
2024-11-22 20:40:14 +1100

    Add initial stack and db monitoring tool

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 const App = () => {
   return (
     <div className="text-center p-4">
-      <h1 className="text-2xl font-bold">Protein Tracker</h1>
+      <h1 className="text-2xl font-bold">Woolies Watcher</h1>
     </div>
   );
 };
