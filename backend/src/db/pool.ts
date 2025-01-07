2e2306b44ca4aed10af4717902f0636f4c40b12e
Henry Goodman <henrygoodman114@gmail.com>
2025-01-07 18:07:52 +1100

    Remove ssl mode

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -2,10 +2,6 @@ import { Pool, types } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
 });
 
 types.setTypeParser(1700, (val) => parseFloat(val)); // NUMERIC
